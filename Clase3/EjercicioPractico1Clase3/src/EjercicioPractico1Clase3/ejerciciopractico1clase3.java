ceb06ca9aeb4441d90c7924a258389a04bee30f7
PINKYBLAST <diegolpzcr@gmail.com>
2024-06-05 18:23:46 -0600

    EjercicioPractico1Clase3

---
+++ Clase3/EjercicioPractico1Clase3/src/EjercicioPractico1Clase3/ejerciciopractico1clase3.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package EjercicioPractico1Clase3;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Diego
+ */
+public class ejerciciopractico1clase3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Escriba un programa que permita jugar  Piedra, Papel o Tijera. Debe funcionar N veces.
+        
+        //Intro
+        JOptionPane.showMessageDialog(null, "Bienvenido a Piedra, Papel o Tijeras.");
+        
+        //Preguntar por cuantas veces quiere jugar
+        Integer partidas = Integer.parseInt(JOptionPane.showInputDialog("Indique el numero de partidas que quieras jugar"));
+        
+        //
+        
+        
+       
+        
+    }
+    
+}
